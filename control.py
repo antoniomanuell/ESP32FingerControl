edcdc8b64e1ea932f76e6fc19726efa7f361086f
antoniomanuell <antoniomanuel.iot@outlook.com>
2024-07-11 16:01:22 +0100

    update python script

--- control.py
+++ control.py
@@ -3,16 +3,17 @@ from cvzone.HandTrackingModule import HandDetector
 import cv2
 import time
 
-# Inicialização do Arduino/ESP32 com PyFirmata
-board = Arduino('/dev/ttyUSB0')
+# Inicialização do Arduino/ESP32 com PyFirmata 
+# Certifique-se de substituir /dev/ttyUSB4 pela porta correcta onde o ESP está conectado
+board = Arduino('/dev/ttyUSB4')
 it = util.Iterator(board)
 it.start()
 
-# Pino do LED no ESP32 (pino 2)
+# Pino do LED no ESP32 
 led_pin = 2
 
 # Configuração do detector de mão
-cap = cv2.VideoCapture(0)  # Captura da câmera obs: verifique o index da sua, pode ser 0, 1 ou 3
+cap = cv2.VideoCapture(0)  # Captura da câmera obs: verifique o index da sua câmera, pode ser 0, 1 ou 3
 detector = HandDetector(staticMode=False, maxHands=2, modelComplexity=1, detectionCon=0.5, minTrackCon=0.5)
 
 # Funções para controle do LED
@@ -35,6 +36,7 @@ while True:
     if hands:
         hand1 = hands[0]  # Pega a primeira mão detectada
         fingers_up = detector.fingersUp(hand1)
+        print(f"Dedos levantados: {fingers_up}")  # Mensagem de depuração
 
         # Se todos os dedos estão levantados, acende o LED; caso contrário, apaga
         if fingers_up.count(1) == 5:  # Todos os dedos levantados
@@ -45,5 +47,4 @@ while True:
     cv2.imshow("Image", img)  # Mostra a imagem com as detecções
     cv2.waitKey(1)  # Espera pela tecla para fechar
 
-board.exit()  # Finaliza a conexão com o ESP32 ao sair do programa
-
+board.exit() 
